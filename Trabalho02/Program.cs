1cf2f38cc1ce2784c0374a8a96954a1f4eec3a37
Jarol0709 <jarolbutronsoria@gmail.com>
2022-09-09 23:38:50 -0300

    V4.0

--- Trabalho02/Program.cs
+++ Trabalho02/Program.cs
@@ -15,5 +15,6 @@ class Octopus
     Console.WriteLine("V1.0");  //Jarol01
     Console.WriteLine("V2.0");  //Jarol02
     Console.WriteLine("V3.0");  //Jarol01
+    Console.WriteLine("V4.0");  //Jarol01
   }
 }
