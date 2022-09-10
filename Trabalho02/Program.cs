01c773d5ba3ea1f5625a72c6253b144f736ca35d
Jarol0709 <jarolbutronsoria@gmail.com>
2022-09-09 22:38:27 -0300

    Commit

---
+++ Trabalho02/Program.cs
@@ -0,0 +1,15 @@
+﻿var o = new Octopus("Jack");
+
+Console.WriteLine(o.Name);  // Jack  
+o.Legs = 20;                // Erro de compilação
+
+class Octopus
+{
+  public readonly string Name;
+  public readonly int Legs = 8;
+  
+  public Octopus(string name)
+  {
+    Name = name;
+  }
+}
